50f4145effa0683ecfda59567b716733900f3f8c
Jagsray <32194460+Jagsray@users.noreply.github.com>
2017-09-25 18:11:01 +0530

    Add files via upload

--- NewTest.java
+++ NewTest.java
@@ -25,36 +25,63 @@ public void spicejetBooking() throws InterruptedException {
 	  Booking.findElement(By.id("ctl00_mainContent_ddl_destinationStation1_CTXT")).sendKeys("HYD",Keys.ENTER);
 	  Booking.findElement(By.className("ui-datepicker-trigger")).click();
 	  Booking.findElement(By.id("ctl00_mainContent_ddl_Adult")).click();
+	  
+	  //Dropdown Actions
 	  WebElement MembersDropdown = Booking.findElement(By.id("ctl00_mainContent_ddl_Adult"));
 	  Select no = new Select(MembersDropdown);
 	  no.selectByIndex(2) ;
 	  WebElement Currency = Booking.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));
 	  Select rs= new Select(Currency);
 	  rs.selectByIndex(1);
+	  
+	  //Checkbox
 	  Booking.findElement(By.id("ctl00_mainContent_chk_IndArm")).click();
 	  Booking.findElement(By.id("ctl00_mainContent_btn_FindFlights")).click();
 	  
+	  //Wait statements
+	  Thread.sleep(6000);
+	  Booking.findElement(By.name("ControlGroupSelectView$AvailabilityInputSelectView$CheckBoxDefenseAgreement")).click();;
+	  Booking.findElement(By.id("ControlGroupSelectView_AgreementInputSelectView_CheckBoxAgreement")).click();
+	  Thread.sleep(1000);
+	  Booking.findElement(By.id("ControlGroupSelectView_ButtonSubmit")).click();
+	  Thread.sleep(4000);
 	  
+	  //Dropdown actions
+	  Booking.findElement(By.id("ControlGroupLoginModelPopUpView_MemberLoginView_TextBoxUserID")).sendKeys("912309875423");
+	  WebElement gender=Booking.findElement(By.id("CONTROLGROUPPASSENGER_ContactInputPassengerView_DropDownListTitle"));
+	  Select gen = new Select(gender);
+	  gen.selectByIndex(3);
+	  
+	  Booking.findElement(By.id("CONTROLGROUPPASSENGER_ContactInputPassengerView_TextBoxFirstName")).sendKeys("Sandeep");
+	  Booking.findElement(By.id("CONTROLGROUPPASSENGER_ContactInputPassengerView_TextBoxLastName")).sendKeys("sen");
+	  Booking.findElement(By.id("CONTROLGROUPPASSENGER_ContactInputPassengerView_TextBoxCity")).sendKeys("Bangaluru");
+	  Booking.findElement(By.id("CONTROLGROUPPASSENGER_ContactInputPassengerView_TextBoxHomePhone")).sendKeys("9087123454");;
+	  Booking.findElement(By.id("CONTROLGROUPPASSENGER_ContactInputPassengerView_TextBoxEmailAddress")).sendKeys("AB@gmail.com");
+	  Booking.findElement(By.id("CONTROLGROUPPASSENGER_ContactInputPassengerView_CheckBoxPrevVisit")).click();
+	  Booking.findElement(By.id("CONTROLGROUPPASSENGER_ContactInputPassengerView_CheckBoxPromoOpt")).click();
+	  
+	  //closing the current window
+	 	  Booking.close();
 	
 	  
   }
   
   @Test
-  public void PersonalDetails()
+  public void PersonalDetails() throws InterruptedException
   {
-	WebDriver Data = new FirefoxDriver();
-	Data.get("http://www.spicejet.com/");
-	Data.navigate().to("https://book.spicejet.com/Contact.aspx");
-	Data.findElement(By.id("ControlGroupLoginModelPopUpView_MemberLoginView_TextBoxUserID")).click();
-	Data.findElement(By.id("ControlGroupLoginModelPopUpView_MemberLoginView_TextBoxUserID")).sendKeys("912345678907");
+	  WebDriver Booking = new FirefoxDriver();
+	  Thread.sleep(1000);
+	  Booking.get("http://www.spicejet.com/");
+	  
+	 
+	  
+	  
+	  Booking.get("http://www.samsung.com/us/");
+	//Closing the entire browser
+	  
+	Booking.quit();
 	  
   }
-	{}
-	
-	{}
-	{}
-	{}
-	Data.close();
 }
 
 
